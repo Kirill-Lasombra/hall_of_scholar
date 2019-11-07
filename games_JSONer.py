e375ad129590d8278cb253ac7f817fb46df77b42
Kirill-Lasombra <49075089+Kirill-Lasombra@users.noreply.github.com>
2019-11-07 11:32:05 +0300

    Оптимизирован код, улучшено выведение текстовой информации, убраны пустые годы.

--- games_JSONer.py
+++ games_JSONer.py
@@ -1,30 +1,28 @@
 import json
 import requests
 import time
+import os, sys
 
 dd = int(input('Укажите день: '))
 mm = int(input('Укажите месяц: '))
 print("Формирую файл со всеми играми вышедшими {:02}.{:02}".format(dd,mm))
-yy=1980
 
-for yy in range(1980,2020):
+for yy in range(1988,2020):
     urls= 'https://api.games.mail.ru/pc/calendar/?date={:04}-{:02}-{:02}&page=1&filter=popular&limit=100'.format(yy,mm,dd)
     response = requests.get(urls)
     today = json.loads(response.text)
-    fl = str('{:02}.{:02}'.format(dd,mm)) + '.txt'
-    with open(fl, "a") as td_fl:
-        print("\n\nИгры " + str(yy) + " года: \n", file=td_fl)
-
-    yy+=1
-    rangelist = range(today['count'])
-    aa = 0
-    for aa in rangelist:
-        name = today['results'][aa]['name'],
-        descr = today['results'][aa]['descr'],
-        game = name + descr
-        aa += 1
+    fl = '{:02}.{:02}.txt'.format(dd,mm)
+    if today['count'] != 0:
 
         with open(fl, "a") as td_fl:
-            print(game, file=td_fl)
-    time.sleep (1)
-    print ("The " + str(yy) + " is ready")
+            print("Игры {:04} года: \n".format(yy), file=td_fl)
+
+        for aa in today['results']:
+            with open(fl, "a") as td_fl:
+                print(aa['name'], file=td_fl)
+                print(aa['descr'], file=td_fl)
+                print('\n', file=td_fl)
+        with open(fl, "a") as td_fl:
+            print('\n', file=td_fl)
+
+    print ("The {:04} year is ready!".format(yy))
