7a89d3ee91ffccb7126ed4acacdf5054bbb9886e
Kirill-Lasombra <49075089+Kirill-Lasombra@users.noreply.github.com>
2019-11-07 06:04:35 +0300

    интерактивный ввод даты, генерация текстового файла, генерация и перебор по годам

--- games_JSONer.py
+++ games_JSONer.py
@@ -1,7 +1,31 @@
 import json
 import requests
+import time
 
-response = requests.get("https://api.games.mail.ru/pc/calendar/?date=2018-11-05&page=1&filter=popular&limit=30")
-todos = json.loads(response.text)
+dd = int(input('Укажите день: '))
+mm = int(input('Укажите месяц: '))
+print("Формирую файл со всеми играми вышедшими " + str('{:02}'.format(dd)) + '.' + str('{:02}'.format(mm)))
+yy=1980
 
-print (todos)
+for yy in range(1988,2020):
+    urls= 'https://api.games.mail.ru/pc/calendar/?date=' + str(yy) + '-' + str('{:02}'.format(mm)) + '-' + str('{:02}'.format(dd))+'&page=1&filter=popular&limit=30'
+    response = requests.get(urls)
+    today = json.loads(response.text)
+    file= str('{:02}'.format(dd))+ '.' + str('{:02}'.format(mm)) + '.txt'
+    fl = str('{:02}'.format(dd)) + '.' + str('{:02}'.format(mm)) + '.txt'
+    with open(fl, "a") as td_fl:
+        print("\n\nИгры " + str(yy) + " года: \n", file=td_fl)
+
+    yy+=1
+    rangelist = range(today['count'])
+    aa = 0
+    for aa in rangelist:
+        name = today['results'][aa]['name'],
+        descr = today['results'][aa]['descr'],
+        game = name + descr
+        aa += 1
+
+        with open(fl, "a") as td_fl:
+            print(game, file=td_fl)
+    time.sleep (1)
+    print ("The " + str(yy) + " is ready")
