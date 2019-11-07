c98c274cad2f0aafbd2eb4e66c83c155fdbb6297
Kirill-Lasombra <49075089+Kirill-Lasombra@users.noreply.github.com>
2019-11-07 07:09:12 +0300

    убран баг с ошибкой, если элементов больше 30. Поправлены выражения через формат

--- games_JSONer.py
+++ games_JSONer.py
@@ -4,15 +4,14 @@ import time
 
 dd = int(input('Укажите день: '))
 mm = int(input('Укажите месяц: '))
-print("Формирую файл со всеми играми вышедшими " + str('{:02}'.format(dd)) + '.' + str('{:02}'.format(mm)))
+print("Формирую файл со всеми играми вышедшими {:02}.{:02}".format(dd,mm))
 yy=1980
 
-for yy in range(1988,2020):
-    urls= 'https://api.games.mail.ru/pc/calendar/?date=' + str(yy) + '-' + str('{:02}'.format(mm)) + '-' + str('{:02}'.format(dd))+'&page=1&filter=popular&limit=30'
+for yy in range(1980,2020):
+    urls= 'https://api.games.mail.ru/pc/calendar/?date={:04}-{:02}-{:02}&page=1&filter=popular&limit=100'.format(yy,mm,dd)
     response = requests.get(urls)
     today = json.loads(response.text)
-    file= str('{:02}'.format(dd))+ '.' + str('{:02}'.format(mm)) + '.txt'
-    fl = str('{:02}'.format(dd)) + '.' + str('{:02}'.format(mm)) + '.txt'
+    fl = str('{:02}.{:02}'.format(dd,mm)) + '.txt'
     with open(fl, "a") as td_fl:
         print("\n\nИгры " + str(yy) + " года: \n", file=td_fl)
 
