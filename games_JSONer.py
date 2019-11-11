910bbb2f8b694226897597cb56da2c3506de8020
Kirill-Lasombra <49075089+Kirill-Lasombra@users.noreply.github.com>
2019-11-11 12:21:55 +0300

    из описаний убраны тэги
    Можно приступать к работе над джанго объектами

--- games_JSONer.py
+++ games_JSONer.py
@@ -1,6 +1,7 @@
 import json
 import requests
-import time
+from bs4 import BeautifulSoup
+import re
 
 dd = int(input('Укажите день: '))
 mm = int(input('Укажите месяц: '))
@@ -22,14 +23,20 @@ for yy in range(1980,2020):
         for aa in today['results']:
             with open(fl, "a") as td_fl:
                 print(aa['name'], file=td_fl)
-                print(aa['descr'], file=td_fl)
+                tag= BeautifulSoup(aa['descr'], features="lxml")
+                print(tag.get_text(), file=td_fl) ## без этого мува описание остаётся с хтмл тегами
                 print (aa['rel_by_reg'][0]['region_list'][0]['name'], file=td_fl)
-                print ([platform['name'] for platform in aa['rel_by_reg'][0]['platform_list']], file=td_fl)
+                print ([platform['name'] for platform in aa['rel_by_reg'][0]['platform_list']], file=td_fl) ## выводит платформы некрасивым списком, но удобно ляжет в бд
+                ##plat= [platform['name'] for platform in aa['rel_by_reg'][0]['platform_list']]
+                ##for bb in plat:
+                    ##print (plat.pop(), file=td_fl)
+                    ##print (' ', file=td_fl)
+                print ('https://games.mail.ru/pc/games/{}'.format(aa['id']), file=td_fl)
 
-
-                ##print(aa['rel_by_reg']['platform_list']['name'], file=td_fl)
                 print('\n', file=td_fl)
         with open(fl, "a") as td_fl:
             print('\n', file=td_fl)
 
+
+    ##time.sleep (1)
     print ("The {:04} year is ready!".format(yy))
