d48b9a994078c24aa04888aeefe45c2d62a58ceb
Kirill-Lasombra <49075089+Kirill-Lasombra@users.noreply.github.com>
2019-11-11 12:20:14 +0300

    В каждую запись добавил зону релиза и платформы
    платформы добавляются в кавычках и скобках. Пофиксить
    добавить в цикл проверку платформ для записи только ПК, ПС4 и Хбох Ван версий.

--- games_JSONer.py
+++ games_JSONer.py
@@ -1,17 +1,18 @@
 import json
 import requests
 import time
-import os, sys
 
 dd = int(input('Укажите день: '))
 mm = int(input('Укажите месяц: '))
 print("Формирую файл со всеми играми вышедшими {:02}.{:02}".format(dd,mm))
 td_fl= open ('{:02}.{:02}.txt'.format(dd,mm),'w')
 
-for yy in range(1988,2020):
+for yy in range(1980,2020):
     urls= 'https://api.games.mail.ru/pc/calendar/?date={:04}-{:02}-{:02}&page=1&filter=popular&limit=100'.format(yy,mm,dd)
     response = requests.get(urls)
     today = json.loads(response.text)
+    lim = today['count']
+    urls = 'https://api.games.mail.ru/pc/calendar/?date={:04}-{:02}-{:02}&page=1&filter=popular&limit={}'.format(yy,mm, dd,lim)
     fl = '{:02}.{:02}.txt'.format(dd,mm)
     if today['count'] != 0:
 
@@ -22,6 +23,11 @@ for yy in range(1988,2020):
             with open(fl, "a") as td_fl:
                 print(aa['name'], file=td_fl)
                 print(aa['descr'], file=td_fl)
+                print (aa['rel_by_reg'][0]['region_list'][0]['name'], file=td_fl)
+                print ([platform['name'] for platform in aa['rel_by_reg'][0]['platform_list']], file=td_fl)
+
+
+                ##print(aa['rel_by_reg']['platform_list']['name'], file=td_fl)
                 print('\n', file=td_fl)
         with open(fl, "a") as td_fl:
             print('\n', file=td_fl)
